c87eacf122da576feb6092410c65438402afb40d
maryamshkk <maryamshk117@gmail.com>
2026-08-16 03:41:20 +0500

    made api for user login authentiction

---
+++ laravel-rest-api/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|min:3',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => bcrypt($validated['password']),
+        ]);
+
+        return response()->json([
+            'message' => 'user created successfully',
+            'user' => $user,
+        ], 201);
+
+    }
+    public function update(Request $request, $id)
+    {
+        $user = User::FindOrFail($id);
+
+        $validated = $request->validate([
+            'name' => 'required|min:3',
+            'password' => 'nullable|min:6',
+        ]);
+
+        $user->name = $validated['name'];
+
+            if($request->filled('password')){
+                $user->password = bcrypt($validated['password']);
+            }
+            
+        $user->save();
+        return response()->json([
+            "message" => "User updated successfully",
+            "user" => $user,
+        ]);
+    }
+        public function destroy(Request $request, $id)
+        {   
+            $user = user::FindOrFail($id);
+            $user->delete();
+
+            return response()->json([
+                "message" => "User deleted successfully"
+            ]);
+
+        }
+}
